f254a29c9b68fd9d88db37cbe39658db55b02e13
ragnar <ragnarr09@ru.is>
2014-05-07 22:50:18 +0000

    Added first version of project

---
+++ TestApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Hello world!";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Smuuu";
+
+			return View();
+		}
+	}
+}
